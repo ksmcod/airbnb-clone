f5edbcb607427218df61037b67712aee5f393ad6
Kono Steve <stevekono77@gmail.com>
2024-08-09 04:24:01 +0100

    created and filled prisma schema

--- src/app/components/modals/Modal.tsx
+++ src/app/components/modals/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
   onSubmit: () => void;
   title?: string;
   body?: React.ReactElement;
-  footer: React.ReactElement;
+  footer?: React.ReactElement;
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;

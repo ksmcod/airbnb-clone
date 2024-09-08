9b1e056ca3960ec5f7a5b4d3b049a2a508f5a7cc
Kono Steve <stevekono77@gmail.com>
2024-09-08 20:24:59 +0100

    created db action to get reservations from db

---
+++ src/app/actions/getReservations.ts
@@ -0,0 +1,41 @@
+import client from "@/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: IParams) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    const query: any = {};
+
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await client.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return reservations;
+  } catch (error: any) {
+    console.log("ERROR: ", error);
+  }
+}
